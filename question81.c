47f2f1413afe491c7f054f461be90f4420b65188
sudhanshu yadav <sudhanshuy991@gmail.com>
2026-06-21 21:58:48 +0530

    question81

---
+++ question81.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        i++;
+    }
+
+    printf("Length of string = %d\n", i);
+
+    return 0;
+}
